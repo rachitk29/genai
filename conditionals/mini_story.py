a8863acffdef896f24a062bd34620e55bde4d08d
rachitk29 <rachitkumar2953@gmail.com>
2025-10-05 18:59:31 +0530

    add new files

---
+++ conditionals/mini_story.py
@@ -0,0 +1,11 @@
+# You're creating a notification sys for a smart kettle 
+# it should remind the user only when the kettle has finished boiling. Task :
+
+# A variable Kettle_boiled = True
+# If boiled, show : "Kettle done! Time to make chai!" 
+
+kettle_boiled = True
+
+if kettle_boiled:
+    print("Kettle done! time to make chai") 
+

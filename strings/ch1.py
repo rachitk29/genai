ef13e4cfb59c5298662fa564b9a5bc378019b64c
rachitk29 <rachitkumar2953@gmail.com>
2025-12-25 13:36:14 +0530

    new files

---
+++ strings/ch1.py
@@ -0,0 +1,11 @@
+word1 = "i love"
+word2 = "python"
+sentence = ("hello" +" " + word1 + " " + word2)
+#concatenate
+print(sentence)
+
+word = "python"
+print(word[3])
+
+for ch in word:
+    print(ch)

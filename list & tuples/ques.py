063d618b5f818e75514b835a70a0f702f0286828
rachitk29 <rachitkumar2953@gmail.com>
2026-01-08 21:33:13 +0530

    adding new files

--- list & tuples/ques.py
+++ list & tuples/ques.py
@@ -14,3 +14,16 @@ for tup in info:
     courses.add((tup[1])) #courses
 
 print(courses)
+
+for name, course in info:
+    if(course == "English"):
+        print(name)
+
+
+dict = {}
+
+for name, course in info:
+    if(dict.get(name) == None):
+        dict.update({name: set()})
+        dict[name].add(course)
+        
